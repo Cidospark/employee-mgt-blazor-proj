8ff539eb8d6e3b34220e21a2e2da31caf7a722bc
Cidospark <cidospark27@gmail.com>
2022-12-17 07:17:26 +0100

    git ignore added

--- BlazorApp/EmpMgt.Web/Pages/EmployeeListBase.cs
+++ BlazorApp/EmpMgt.Web/Pages/EmployeeListBase.cs
@@ -17,6 +17,13 @@ namespace EmpMgt.Web.Pages
 		{
             System.Threading.Thread.Sleep(2000);
 
+            var deptList = new List<Department>
+            {
+                new Department { DepartmentId = 1, DepartmentName = "Marketing" },
+                new Department { DepartmentId = 2, DepartmentName = "Designs" },
+                new Department { DepartmentId = 3, DepartmentName = "Product" }
+            };
+
 			Employees = new List<Employee>
 			{
 				new Employee
@@ -27,7 +34,7 @@ namespace EmpMgt.Web.Pages
 					Email = "maryl@sample.com",
 					DateOfBirth = new DateTime(1982, 1, 19),
 					Gender = Data.Enums.Gender.Female,
-					Department = new Department { DepartmentId = 1, DepartmentName = "Marketing" },
+					Department = deptList[0].DepartmentId,
 					Photo = "images/mary.png"
 				},
                 new Employee
@@ -38,7 +45,7 @@ namespace EmpMgt.Web.Pages
                     Email = "johnl@sample.com",
                     DateOfBirth = new DateTime(1989, 10, 10),
                     Gender = Data.Enums.Gender.Male,
-                    Department = new Department { DepartmentId = 1, DepartmentName = "Marketing" },
+                    Department = deptList[0].DepartmentId,
                     Photo = "images/john.png"
                 },
                 new Employee
@@ -49,7 +56,7 @@ namespace EmpMgt.Web.Pages
                     Email = "sarar@sample.com",
                     DateOfBirth = new DateTime(1994, 11, 1),
                     Gender = Data.Enums.Gender.Female,
-                    Department = new Department { DepartmentId = 2, DepartmentName = "Designs" },
+                    Department = deptList[1].DepartmentId,
                     Photo = "images/sara.png"
                 },
                 new Employee
@@ -60,7 +67,7 @@ namespace EmpMgt.Web.Pages
                     Email = "samf@sample.com",
                     DateOfBirth = new DateTime(1984, 1, 6),
                     Gender = Data.Enums.Gender.Male,
-                    Department = new Department { DepartmentId = 3, DepartmentName = "Product" },
+                    Department = deptList[2].DepartmentId,
                     Photo = "images/sam.jpeg"
                 }
             };
