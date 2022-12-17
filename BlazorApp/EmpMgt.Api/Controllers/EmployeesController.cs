63e6ab7fd9f3909b7c00eff023ec608a1a7ee7b8
Cidospark <cidospark27@gmail.com>
2022-12-17 08:13:15 +0100

    implemented get actions for all employees and single employee

---
+++ BlazorApp/EmpMgt.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,50 @@
+﻿using EmpMgt.Data.Entities;
+using EmpMgt.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpMgt.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeesController : ControllerBase
+{
+    private readonly IEmployeeRepository employeeRepository;
+
+    public EmployeesController(IEmployeeRepository employeeRepository)
+    {
+        this.employeeRepository = employeeRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetEmployees()
+    {
+        try
+        {
+            return Ok(await employeeRepository.GetEmployees());
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error retrieving data from the database");
+        }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Employee>> GetEmployee(int id)
+    {
+        try
+        {
+            var result = await employeeRepository.GetEmployee(id);
+
+            if (result == null) return NotFound();
+
+            return result;
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error retrieving data from the database");
+        }
+    }
+}
+
