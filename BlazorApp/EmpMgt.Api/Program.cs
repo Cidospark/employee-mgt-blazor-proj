bc6bfd3b0810284dda8b21a4e582df265a3bdd12
Cidospark <cidospark27@gmail.com>
2022-12-17 02:44:38 +0100

    Connection string added and registered

--- BlazorApp/EmpMgt.Api/Program.cs
+++ BlazorApp/EmpMgt.Api/Program.cs
@@ -1,4 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using EmpMgt.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"), x => x.MigrationsAssembly("EmpMgt.Data"));
+});
 
 // Add services to the container.
 
